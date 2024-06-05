33792568d0dc357164a8ad1e1ec3ac6f24907d3d
B33blebroxx <b33blebroxx@gmail.com>
2024-06-05 12:38:47 -0500

    Implemented remove performer from show functionality

--- components/Cards/PerformerCard.js
+++ components/Cards/PerformerCard.js
@@ -1,22 +1,39 @@
+import { Button } from '@mui/material';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
+import { useContext } from 'react';
 import { Image } from 'react-bootstrap';
+import { UserContext } from '../../utils/context/authContext';
 
-export default function PerformerCard({ performer }) {
+export default function PerformerCard({ performer, onRemove, showPromotionId }) {
+  const { user } = useContext(UserContext);
+
+  const handleRemove = () => {
+    onRemove(performer.id);
+  };
+
   return (
-    <Link href={`/performers/profile/${performer?.id}`}>
-      <div className="performer-card">
-        <Image src={performer?.image} style={{ height: '27rem', width: '19rem' }} alt="Performer Image" />
-        <div className="performer-card-info">
-          <h4>{performer?.ringName}</h4>
-          <p>{performer?.role.title}</p>
+    <div className="d-flex flex-column align-items-center mb-5">
+      <Link href={`/performers/profile/${performer?.id}`} passHref>
+        <div className="performer-card">
+          <Image src={performer?.image} style={{ height: '27rem', width: '19rem' }} alt="Performer Image" />
+          <div className="performer-card-info">
+            <h4>{performer?.ringName}</h4>
+            <p>{performer?.role.title}</p>
+          </div>
         </div>
-      </div>
-    </Link>
+      </Link>
+      {showPromotionId === user.promotionId && (
+        <div>
+          <Button variant="outlined" color="error" onClick={handleRemove}>Remove Performer</Button>
+        </div>
+      )}
+    </div>
   );
 }
 
 PerformerCard.propTypes = {
+  showPromotionId: PropTypes.number.isRequired,
   performer: PropTypes.shape({
     id: PropTypes.number.isRequired,
     ringName: PropTypes.string.isRequired,
@@ -26,4 +43,5 @@ PerformerCard.propTypes = {
     }).isRequired,
     image: PropTypes.string.isRequired,
   }).isRequired,
+  onRemove: PropTypes.func.isRequired,
 };
